ff6eddad09cd51f3ca6e080b574a165446552378
pczernic <105754562+pczernic@users.noreply.github.com>
2022-10-25 22:57:55 +0000

    cleaning

--- gpr_mls/run_livox.sh
+++
@@ -1,9 +0,0 @@
-cd /home/jgorka/catkin_fastlio_slam
-source ./devel/setup.bash
-roscore &
-sleep 2
-roslaunch fast_lio mapping_avia.launch &
-sleep 2 
-roslaunch livox_ros_driver livox_lidar_msg.launch &
-sleep 2
-rosbag record -a -o session.bag
